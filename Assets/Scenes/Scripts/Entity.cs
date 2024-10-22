0b94b710a2073160f17eee2df79e1c539e1c89d5
Destroh33 <krishna.tholudur@gmail.com>
2024-10-22 15:42:29 -0700

    camera changes

--- Assets/Scenes/Scripts/Entity.cs
+++ Assets/Scenes/Scripts/Entity.cs
@@ -6,11 +6,33 @@ using UnityEngine.Events;
 
 public class Entity : MonoBehaviour
 {
+    [NonSerialized]
+    public float CurrentHealth;
+    bool dead = false;
     [Serializable]
     public struct EntityStats
     {
+        public float MaxHealth;
         public float MovementSpeed;
+        
+    }
+    private void Start()
+    {
+        CurrentHealth = stats.MaxHealth;
     }
 
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= Mathf.Ceil(damage);
+        if(CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        dead = true;
+        Destroy(gameObject);
+    }
     public EntityStats stats;
 }
